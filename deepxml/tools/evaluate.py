bb8cb36ff03e634f4d1caa888145ea84dae4422a
kunaldahiya <kunalsdahiya@gmail.com>
2020-04-07 16:24:08 +0530

    Added filtering

--- deepxml/tools/evaluate.py
+++ deepxml/tools/evaluate.py
@@ -2,8 +2,9 @@
 import sys
 import xclib.evaluation.xc_metrics as xc_metrics
 import xclib.data.data_utils as data_utils
-from scipy.io import loadmat,savemat
+from scipy.io import loadmat, savemat
 from scipy.sparse import lil_matrix, load_npz, csr_matrix
+from xclib.utils.sparse import sigmoid
 import scipy.sparse as sparse
 import numpy as np
 import copy
@@ -11,58 +12,65 @@ import os
 import torch
 
 
-def sigmoid(mat):
-    mat.__dict__['data'] = 1/(np.exp(-mat.__dict__['data'])+1.0)
-    return mat
+def get_filter_map(fname):
+    if fname is not None:
+        return np.loadtxt(fname, dtype=np.int)
+    else:
+        return None
 
-def normalize(mat):
-    mat.__dict__['data'] = np.exp(mat.__dict__['data'])
-    _max = mat.max(axis=1).toarray().ravel()
-    _max[_max == 0] = 1.0
-    _norm = sparse.diags(1.0/_max)
-    return _norm.dot(mat).tocsr()
 
-def main(targets_label_file, train_label_file, predictions_file, A, B, betas, _type):
-    true_labels = data_utils.read_sparse_file(targets_label_file, force_header=True)
-    trn_labels = data_utils.read_sparse_file(train_label_file, force_header=True)
+def filter_predictions(pred, mapping):
+    if mapping is not None:
+        print("Filtering labels.")
+        pred[mapping[:, 0], mapping[:, 1]] = 0
+        pred.eliminate_zeros()
+    return pred
+
+
+def main(targets_label_file, train_label_file, predictions_file,
+         A, B, betas, filter_fname=None):
+    true_labels = data_utils.read_sparse_file(targets_label_file)
+    trn_labels = data_utils.read_sparse_file(train_label_file)
     inv_propen = xc_metrics.compute_inv_propesity(trn_labels, A, B)
-    acc = xc_metrics.Metrices(true_labels, inv_propensity_scores=inv_propen, remove_invalid=False)
+    mapping = get_filter_map(filter_fname)  # get_filter_map(filter_fname)
+    acc = xc_metrics.Metrics(true_labels, inv_psp=inv_propen)
     root = os.path.dirname(predictions_file)
-    if betas[0]!=-1:
-        knn = load_npz(predictions_file+'_knn.npz')
-        clf = load_npz(predictions_file+'_clf.npz')
+    if betas[0] != -1:
+        knn = filter_predictions(
+            load_npz(predictions_file+'_knn.npz'), mapping)
+        clf = filter_predictions(
+            load_npz(predictions_file+'_clf.npz'), mapping)
+
         print("clf")
         args = acc.eval(clf, 5)
         print(xc_metrics.format(*args))
         print("knn")
         args = acc.eval(knn, 5)
         print(xc_metrics.format(*args))
-    
-        if _type == 1:
-            clf = sigmoid(clf)
-            knn = sigmoid(knn)
-        elif _type == 2:
-            clf = normalize(clf)
-            knn = normalize(knn)
+        clf = sigmoid(clf)
+        knn = sigmoid(knn)
         for beta in betas:
             predicted_labels = beta*clf + (1-beta)*knn
             args = acc.eval(predicted_labels, 5)
-            print("beta %f, Method %d" % (beta, _type))
+            print("beta %f" % (beta))
             print(xc_metrics.format(*args))
             # data_utils.write_sparse_file(
             #     predicted_labels, "%s/%s_beta_%f_type_%d.txt" % (root, "score", beta, _type))
     else:
-        predicted_labels = load_npz(predictions_file+'.npz')
+        predicted_labels = filter_predictions(
+            load_npz(predictions_file+'.npz'), mapping)
         args = acc.eval(predicted_labels, 5)
         print(xc_metrics.format(*args))
+
 
 if __name__ == '__main__':
     train_label_file = sys.argv[1]
     targets_file = sys.argv[2]  # Usually test data file
     predictions_file = sys.argv[3]  # In mat format
-    A = float(sys.argv[4])
-    B = float(sys.argv[5])
-    _type=int(sys.argv[6])
+    filter_map = sys.argv[4]
+    A = float(sys.argv[5])
+    B = float(sys.argv[6])
 
     betas = list(map(float, sys.argv[7:]))
-    main(targets_file, train_label_file, predictions_file, A, B, betas, _type)
+    main(targets_file, train_label_file,
+         predictions_file, A, B, betas, filter_map)
